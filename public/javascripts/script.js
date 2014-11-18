36f9c8b042dc4b4385900d44399907fb35416a1f
Ryo Suzuki <ryooopan@googlemail.com>
2014-11-19 00:11:05 +0900

    simple get value

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,6 +3,14 @@ $(function() {
   var editor = ace.edit('editor');
   editor.setTheme('ace/theme/monokai');
   editor.getSession().setMode('ace/mode/javascript');
+
+  var session = editor.getSession();
+
+  session.on('change', function() {
+    var value = session.getValue();
+    console.log(value);
+  });
+
 });
 
 
