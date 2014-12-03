56578e4ff82aa5bd394d8f59971e8d98a07221b1
Ryo Suzuki <ryooopan@googlemail.com>
2014-12-03 12:07:56 +0900

    add steps in left column

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,11 +5,45 @@ $(function() {
   editor.getSession().setMode('ace/mode/javascript');
 
   var session = editor.getSession();
-
+  
   session.on('change', function() {
     var value = session.getValue();
     console.log(value);
   });
+
+  $('#step-1').click( function() {
+    session.insert({row: 2, column: 0}, '\nvar incomeScale = d3.scale.log().domain([100, 100000]).range([0, width]);\nvar lifeScale = d3.scale.linear().domain([10, 90]).range([height, 0]);\nvar populationScale = d3.scale.sqrt() .domain([0, 1e9]).range([1, 50]);\n\n');
+    
+    $('#iframe').contents().find('body').html('step-1 html');
+  });
+
+  $('#step-2').click( function() {
+    session.insert({row: 7, column: 0}, 'var incomeAxis = d3.svg.axis()\n .orient("bottom")\n .scale(incomeScale) .ticks(5, d3.format(",d"));\n\n');
+
+    $('#iframe').contents().find('body').html('step-2 html');
+    
+  });
+
+  $('#step-3').click( function() {
+    session.insert({row: 10, column: 0}, 'var lifeAxis = d3.svg.axis()\n .orient("left")\n .scale(lifeScale);\n\n');
+
+    $('#iframe').contents().find('body').html('step-3 html');
+  });
+
+  $('#step-4').click( function() {
+    session.insert({row: 16, column: 0}, 'var svg = d3.select("body").append("svg")\n .attr("width", width + 2*offset)\n .attr("height", height + 2*offset)\n .attr("transform", "translate(" + offset + "," + offset + ")");');
+
+    $('#iframe').contents().find('body').html('step-4 html');
+  });
+  
+  $('#step-5').click( function() {
+    session.insert({row: 19, column: 0}, 'svg.append("g")\n .attr("class", "incomeAxis")\n .attr("fill", "none")\n .attr("stroke", "black")\n .attr("shape-rendering", "crispEdges") .attr("transform", "translate(0," + height + ")") .call(incomeAxis);');
+
+    $('#iframe').contents().find('body').html('Complete !');
+
+  });
+  
+
 
 });
 
